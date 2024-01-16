19514ce773bba2951733db76fa7b070cfb065e33
jforgue <26842354+bandogora@users.noreply.github.com>
2024-01-16 11:31:33 -0500

    Add site.js to terminal.js for now

--- typescripts/terminal.ts
+++ typescripts/terminal.ts
@@ -1,3 +1,7 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faTerminal } from '@fortawesome/free-solid-svg-icons'
+import { faSimCard } from '@fortawesome/free-solid-svg-icons'
 import { Terminal } from 'xterm';
 import { WebglAddon } from 'xterm-addon-webgl';
 import { FitAddon } from 'xterm-addon-fit';
@@ -5,6 +9,16 @@ import { FitAddon } from 'xterm-addon-fit';
 const terminal = new Terminal();
 const webglAddon = new WebglAddon();
 const fitAddon = new FitAddon();
+
+library.add(faHouse, faSimCard, faTerminal);
+
+if (document.readyState === "loading") {
+  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", onDomReady);
+} else {
+  // `DOMContentLoaded` has already fired
+  onDomReady();
+}
 
 webglAddon.onContextLoss(e => {
   webglAddon.dispose();
@@ -17,12 +31,5 @@ function onDomReady() {
   fitAddon.fit();
 
   terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
-}
-
-if (document.readyState === "loading") {
-  // Loading hasn't finished yet
-  document.addEventListener("DOMContentLoaded", onDomReady);
-} else {
-  // `DOMContentLoaded` has already fired
-  onDomReady();
+  dom.watch();
 }
